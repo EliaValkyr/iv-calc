4f6cc142d112ce6485ac4bf9ac0cc066730949a5
Elia Surrell <elia.surrell@gmail.com>
2021-12-17 15:44:07 +0100

    Created components with basic UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { StatPanel } from './components/StatPanel';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    computeScaledStat(baseStat: number, ev: number, iv: number, level: number): number {
+        const evStat: number = Math.floor(ev / 4)
+        const unscaledStat: number = 2 * baseStat + iv + evStat
+        const scaledStat: number = Math.floor(unscaledStat * level / 100)
+        return scaledStat
+    }
+
+    computeHPStat(baseStat: number, ev: number, iv: number, level: number): number {
+        const hpStat: number = this.computeScaledStat(baseStat, ev, iv, level) + level + 10
+        return hpStat
+    }
+
+    computeOtherStat(baseStat: number, ev: number, iv: number, level: number, natureMult: number): number {
+        const otherStat: number = Math.floor((this.computeScaledStat(baseStat, ev, iv, level) + 5) * natureMult)
+        return otherStat
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="app">
+                    <StatPanel statName="HP" baseStat={100} ev={0} iv={0} natureMult={1}/>
+                    <StatPanel statName="Atk" baseStat={100} ev={0} iv={0} natureMult={1}/>
+                </div>
+            </div >
+        )
+    }
 }
 
 export default App;
