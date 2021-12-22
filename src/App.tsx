e8a28b32424b83ae3bd4a2e2cb69c6f2612bbc95
Elia Surrell <elia.surrell@gmail.com>
2021-12-22 17:25:10 +0100

    Added tab layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import Tabs from '@mui/material/Tabs';
 import './App.css';
 import { AllBaseStatsPanel } from './components/AllBaseStatsPanel';
 import { PokemonMainInfoPanel } from './components/PokemonMainInfoPanel';
 import { MAX_LEVEL } from './constants';
 import { Clamp } from './utils';
+import { Box, Tab, Typography } from '@mui/material';
 
 interface AppProps {
 
@@ -13,33 +16,34 @@ interface AppState {
     species: string,
     level: number,
     natureString: string,
+    currentTab: number,
 }
 
 class App extends React.Component<AppProps, AppState> {
     constructor(props: AppProps) {
         super(props)
 
-        this.state = {species: "Blaziken", level: 70, natureString: "Adamant"}
+        this.state = { species: "Blaziken", level: 70, natureString: "Adamant", currentTab: 0 }
     }
 
     render() {
         const onLevelChanged = (levelValue: number) => {
             const sanitizedlevelValue = Clamp(levelValue, 0, MAX_LEVEL)
-            this.setState({level: sanitizedlevelValue})
+            this.setState({ level: sanitizedlevelValue })
         }
-        
+
         const onSpeciesChanged = (species: string) => {
-            this.setState({species: species})
+            this.setState({ species: species })
         }
-        
+
         const onNatureChanged = (nature: string) => {
-            this.setState({natureString: nature})
+            this.setState({ natureString: nature })
         }
 
         return (
             <div className="container">
                 <div className="app">
-                    <PokemonMainInfoPanel 
+                    <PokemonMainInfoPanel
                         level={this.state.level}
                         species={this.state.species}
                         natureString={this.state.natureString}
@@ -47,11 +51,42 @@ class App extends React.Component<AppProps, AppState> {
                         onSpeciesChanged={onSpeciesChanged}
                         onNatureChanged={onNatureChanged}
                     />
-                    <AllBaseStatsPanel 
-                        level={this.state.level} 
-                        species={this.state.species}
-                        natureString={this.state.natureString}
-                    />
+                    <Box sx={{ width: '100%' }}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <Tabs
+                                variant="fullWidth"
+                                value={this.state.currentTab}
+                                onChange={(e, v) => {
+                                    this.setState({ currentTab: v })
+                                }}
+                            >
+                                <Tab label="IV Calculator" />
+                                <Tab label="Stat Calculator" />
+                            </Tabs>
+                        </Box>
+
+                        <SwipeableViews
+                            axis={'x'}
+                            index={this.state.currentTab}
+                            onChangeIndex={(newTab: number) => {
+                                this.setState({ currentTab: newTab })
+                            }}
+                        >
+                            <AllBaseStatsPanel
+                                hidden={this.state.currentTab !== 0}
+                                level={this.state.level}
+                                species={this.state.species}
+                                natureString={this.state.natureString}
+                            />
+                            <AllBaseStatsPanel
+                                hidden={this.state.currentTab !== 1}
+                                level={this.state.level}
+                                species={this.state.species}
+                                natureString={this.state.natureString}
+                            />
+                        </SwipeableViews>
+
+                    </Box>
                 </div>
             </div >
         )
--- src/components/AllBaseStatsPanel.tsx
+++ src/components/AllBaseStatsPanel.tsx
@@ -6,6 +6,7 @@ import data from '../PokemonData.json'
 import { Nature, natureArray } from '../nature';
 
 export interface AllBaseStatsPanelProps {
+    hidden: boolean,
     species: string,
     level: number,
     natureString: string
@@ -17,22 +18,25 @@ export interface AllBaseStatsPanelState {
 
 export class AllBaseStatsPanel extends React.Component<AllBaseStatsPanelProps, AllBaseStatsPanelState> {
     render() {
-        const {level, species, natureString} = this.props
+        const { hidden, level, species, natureString } = this.props
 
         const pokemonData = data.find(x => x.Species.toLowerCase() == this.props.species.toLowerCase())
 
         const optNature = natureArray.find(x => x.mName == natureString)
         const nature: Nature = optNature == undefined ? new Nature("Dummy") : optNature!
-        
+
         return (
-            <div className="container">
+            <div
+                className="container"
+                hidden={hidden}
+            >
                 <div className="all-base-stats-panel">
-                    <StatPanel statType={StatType.HP} baseStat={pokemonData ? pokemonData!.HP : 1} level={level} nature={nature}/>
-                    <StatPanel statType={StatType.Attack} baseStat={pokemonData ? pokemonData!.Attack : 1} level={level} nature={nature}/>
-                    <StatPanel statType={StatType.Defense} baseStat={pokemonData ? pokemonData!.Defense : 1} level={level} nature={nature}/>
-                    <StatPanel statType={StatType.SpAttack} baseStat={pokemonData ? pokemonData!.SpAttack : 1} level={level} nature={nature}/>
-                    <StatPanel statType={StatType.SpDefense} baseStat={pokemonData ? pokemonData!.SpDefense : 1} level={level} nature={nature}/>
-                    <StatPanel statType={StatType.Speed} baseStat={pokemonData ? pokemonData!.Speed : 1} level={level} nature={nature}/>
+                    <StatPanel statType={StatType.HP} baseStat={pokemonData ? pokemonData!.HP : 1} level={level} nature={nature} />
+                    <StatPanel statType={StatType.Attack} baseStat={pokemonData ? pokemonData!.Attack : 1} level={level} nature={nature} />
+                    <StatPanel statType={StatType.Defense} baseStat={pokemonData ? pokemonData!.Defense : 1} level={level} nature={nature} />
+                    <StatPanel statType={StatType.SpAttack} baseStat={pokemonData ? pokemonData!.SpAttack : 1} level={level} nature={nature} />
+                    <StatPanel statType={StatType.SpDefense} baseStat={pokemonData ? pokemonData!.SpDefense : 1} level={level} nature={nature} />
+                    <StatPanel statType={StatType.Speed} baseStat={pokemonData ? pokemonData!.Speed : 1} level={level} nature={nature} />
                 </div>
             </div>
         )
