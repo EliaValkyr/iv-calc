dcc0d30b86017594b441dc711503a64a378b37ca
Alba Mendez <me@alba.sh>
2021-12-23 19:20:07 +0100

    fix sprite location

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const EV_STEP = 4
 export const MAX_LEVEL = 100
 export const NATURE_INCREASE_MULT = 1.1
 export const NATURE_DECREASE_MULT = 0.9
-export const SPECIES_SPRITE_FOLDER = "resources/sprites/species/"
+export const SPECIES_SPRITE_FOLDER = `${process.env.PUBLIC_URL}/resources/sprites/species/`
